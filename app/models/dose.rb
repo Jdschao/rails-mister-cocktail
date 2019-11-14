4a03ed877b7ce9269cb0ec82ed93a23df6851d84
Johnny Chao <jdschao@gmail.com>
2019-11-14 16:22:05 -0500

    completed model

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,2 +1,7 @@
 class Dose < ApplicationRecord
+    belongs_to :cocktail
+    belongs_to :ingredient
+    validates :description, :cocktail, presence: true
+    validates :ingredient, presence: true
+    validates :cocktail, uniqueness: { scope: :ingredient }
 end
--- db/migrate/20191114192829_create_doses.rb
+++ db/migrate/20191114192829_create_doses.rb
@@ -2,8 +2,8 @@ class CreateDoses < ActiveRecord::Migration[6.0]
   def change
     create_table :doses do |t|
       t.string :description
-      t.string :cocktail
-      t.string :ingredient
+      t.references :cocktail, foreign_key: true
+      t.references :ingredient, foreign_key: true
 
       t.timestamps
     end
